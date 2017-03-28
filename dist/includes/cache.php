b5996b4e1bb244919041b17f1a8d497d4d7abacf
CarinaChenot <mllecarinach@gmail.com>
2017-03-28 18:17:44 +0200

    :sparkles: Added cache

---
+++ dist/includes/cache.php
@@ -0,0 +1,23 @@
+<?php
+
+// Setup
+$astronauts = ['astro_kimbrough', 'Thom_astro', 'AstroPeggy', 'Astro_Alex', 'astro_luca', 'astro_timpeake', 'Astro2fish', 'Astro_Jeff', 'Astro_Wheels'];
+
+foreach ($astronauts as $_astronaut) {
+    $path = './cache/'.md5($_astronaut.date('Y-m-d H'));
+
+    // From cache
+    if (file_exists($path)) {
+
+        $tweets[$_astronaut] = file_get_contents($path);
+
+    // From API
+    } else {
+
+        // Get content
+        $tweets[$_astronaut] = file_get_contents('http://google.com'); // À remplacer par la requête API
+
+        // Save in cache
+        file_put_contents($path, $tweets);
+    }
+}
