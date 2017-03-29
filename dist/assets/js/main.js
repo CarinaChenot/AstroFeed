6221fff8df42eb6a74c2d892261062ab879f0e8f
BereniceDavid <berenice.david@hotmail.fr>
2017-03-29 05:26:56 +0200

    :sparkles: :lipstick: WebGL added space centers markers + changed earth visual aspect

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,13 +1,13 @@
-'use strict';
-
-// Landing
-
-var myVar = void 0;
-
-function timer() {
-  myVar = setTimeout(hidePage, 10000);
-}
-
-function hidePage() {
-  document.getElementById('landing').style.display = 'none';
-}
+//'use strict';
+//
+//// Landing
+//
+//var myVar = void 0;
+//
+//function timer() {
+//  myVar = setTimeout(hidePage, 10000);
+//}
+//
+//function hidePage() {
+//  document.getElementById('landing').style.display = 'none';
+//}
